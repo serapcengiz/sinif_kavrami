8a5ea88db5efd801d006a55c857d62d3718a8c41
serapcengiz <73667009+serapcengiz@users.noreply.github.com>
2022-08-28 17:04:19 +0300

    kurucuyapisi

--- Program.cs
+++ Program.cs
@@ -20,11 +20,7 @@ namespace sinif_kavrami
             //*Internal
             //*Protected
 
-            Calisan calisan1=new Calisan();
-            calisan1.Ad="Ayşe";
-            calisan1.SoyAd="Kara";
-            calisan1.No=234564;
-            calisan1.Departman="İnsan Kaynakları";
+            Calisan calisan1=new Calisan("Ayşe","Kara",234564,"İnsan Kaybakları");
             calisan1.CalisanBilgileri();
 
             Calisan calisan2=new Calisan();
@@ -34,6 +30,8 @@ namespace sinif_kavrami
             calisan2.Departman="Satın Alma";
             calisan2.CalisanBilgileri();
 
+            Calisan calisan3=new Calisan("Serap","Cengiz");
+            calisan3.CalisanBilgileri();
 
         }
     }
@@ -43,6 +41,19 @@ namespace sinif_kavrami
         public string SoyAd;
         public int No;
         public string Departman;
+        public Calisan(string ad, string soyad,int no,string departman)
+        {
+            this.Ad=ad;
+            this.SoyAd=soyad;
+            this.No=no;
+            this.Departman=departman;
+        }
+        public Calisan(string ad, string soyad)
+        {
+            this.Ad=ad;
+            this.SoyAd=soyad;
+        }
+        public Calisan(){}
         public void CalisanBilgileri()
         {
             Console.WriteLine("Çalışan Adı {0}",Ad);
